4add447ffb23da03f3ead68c089f89194f6b6e30
Ryan Flach <ryanflach@users.noreply.github.com>
2016-06-19 20:26:40 -0700

    I4: guess limiting added for second extension

--- web_guesser.rb
+++ web_guesser.rb
@@ -2,27 +2,51 @@ require 'sinatra'
 require 'sinatra/reloader'
 
 set :SECRET_NUMBER, rand(100)
+@@guesses = 5
 
 get '/' do
   guess = params["guess"]
-  message = compare_guess(guess)
+  message = process_guess(guess)
   color = get_color(message)
-  erb :index, :locals => {:number => settings.SECRET_NUMBER, :message => message, :color => color}
+  erb :index, :locals => {:number => secret_number, :message => message, :color => color}
+end
+
+def secret_number
+  settings.SECRET_NUMBER
+end
+
+def process_guess(guess)
+  @@guesses -= 1
+  message = compare_guess(guess)
+  if message[0..2] == "You"
+    reset
+    message
+  elsif @@guesses == 0
+    reset
+    "You lost. #{@@guesses} guesses remaining in a new game."
+  else
+    message
+  end
+end
+
+def reset
+  @@guesses = 5
+  settings.SECRET_NUMBER = rand(100)
 end
 
 def compare_guess(guess)
   return "Please guess a number." if guess.nil?
   guess = guess.to_i
-  if guess > SECRET_NUMBER + 5
+  if guess > secret_number + 5
     "Way too high!"
-  elsif guess < SECRET_NUMBER - 5
+  elsif guess < secret_number - 5
     "Way too low!"
-  elsif guess > SECRET_NUMBER
+  elsif guess > secret_number
     "Too high!"
-  elsif guess < SECRET_NUMBER
+  elsif guess < secret_number
     "Too low!"
   else
-    "You got it right!<br><br>The SECRET NUMBER is #{SECRET_NUMBER}."
+    "You got it right!<br><br>The SECRET NUMBER was #{secret_number}.<br><br>The number has been reset."
   end
 end
 
@@ -31,9 +55,7 @@ def get_color(guess_outcome)
     "#E8000A"
   elsif guess_outcome == "Too high!" || guess_outcome == "Too low!"
     "#FFA197"
-  elsif guess_outcome[0..2] == "You"
+  elsif guess_outcome[0..6] == "You got"
     "#00FF12"
-  else
-    ""
   end
 end
